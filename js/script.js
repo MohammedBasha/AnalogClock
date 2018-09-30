f8f545044f52d401e2dd54d8ed344c629052676a
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-01 00:30:28 +0200

    Making an Date Object instance and get the time details

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-// Grab the clock hands
+// Grab the Clock Hands
 const HOURHAND = document.querySelector("#hour"),
       MINUTEHAND = document.querySelector("#minute"),
       SECONDHAND = document.querySelector("#second");
 
-// Set the hands positions
+// Creates a JavaScript Date Object instance 
+var date = new Date();
+
+// Defining Variables for the Clock Hands Positions
 let hrPosition = 20,
     minPosition = 130,
-    secPosition = 267;
+    secPosition = 267,
+    // Defining Variables for the Time
+    hr = date.getHours(),
+    min = date.getMinutes(),
+    sec = date.getSeconds();
 
+// Set the Clock Hands Transform Styles
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
 SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
