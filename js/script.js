4f817838fb0b2c59d0c7d89e57618d6190c4c0a8
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-01 00:03:13 +0200

    Controlling the Clock Hands with JavaScript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-const HOURHAND = document.querySelector("#hour");
-const MINUTEHAND = document.querySelector("#minute");
-const SECONDHAND = document.querySelector("#second");
+// Grab the clock hands
+const HOURHAND = document.querySelector("#hour"),
+      MINUTEHAND = document.querySelector("#minute"),
+      SECONDHAND = document.querySelector("#second");
+
+// Set the hands positions
+let hrPosition = 20,
+    minPosition = 130,
+    secPosition = 267;
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
