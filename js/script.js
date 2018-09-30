b1a274f6246c7281921df08143318cabb8dc1ece
Mohammed Basha <mohammedbasha@bestresponsemedia.com>
2018-10-01 01:07:56 +0200

    Start the clock hands movements second by second

--- js/script.js
+++ js/script.js
@@ -3,21 +3,25 @@ const HOURHAND = document.querySelector("#hour"),
       MINUTEHAND = document.querySelector("#minute"),
       SECONDHAND = document.querySelector("#second");
 
-// Creates a JavaScript Date Object instance 
-var date = new Date();
+function runTheClock() {
+    // Creates a JavaScript Date Object instance 
+    var date = new Date();
 
-let 
-    // Defining Variables for the Time
-    hr = date.getHours(),
-    min = date.getMinutes(),
-    sec = date.getSeconds(),
-    
-    // Defining Variables for the Clock Hands Positions
-    hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12),
-    minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60),
-    secPosition = sec * 360 / 60;
+    let 
+        // Defining Variables for the Time
+        hr = date.getHours(),
+        min = date.getMinutes(),
+        sec = date.getSeconds(),
 
-// Set the Clock Hands Transform Styles
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+        // Defining Variables for the Clock Hands Positions
+        hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12),
+        minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60),
+        secPosition = sec * 360 / 60;
+
+    // Set the Clock Hands Transform Styles
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runTheClock, 500);
